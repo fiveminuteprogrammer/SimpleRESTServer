0eeeefa7ad3a518233dbf6e25415fe01954b6867
jensenjs <jensenjs@LEGOLAS64>
2016-06-27 16:39:17 -0600

    Add project files.

---
+++ SimpleRESTServer/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+
+namespace SimpleRESTServer
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
